570c28509ff633195d7af49dcb0c65609877ef2b
Alexey Tsymbal <alexey@gotiggy.com>
2022-02-04 01:42:35 +0300

    Integrate multiple pipelines for single parameter

--- src/client/jet-client.ts
+++ src/client/jet-client.ts
@@ -5,7 +5,7 @@ import { programs, sleep, Wallet_ } from '@dialectlabs/web3';
 import { createDialect, deleteDialect, idl } from '@dialectlabs/web3';
 import { Duration } from 'luxon';
 
-const JET_PUBLIC_KEY = process.env.c as string;
+const JET_PUBLIC_KEY = process.env.JET_PUBLIC_KEY as string;
 
 const NETWORK_NAME = 'localnet';
 const connection = new web3.Connection(
--- src/server/jet-pipeline.ts
+++ src/server/jet-pipeline.ts
@@ -7,7 +7,12 @@ const collateralizationRatioNotifications: EventDetectionPipeline<number> = (
   source,
 ) =>
   source
-    .pipe(Operators.Utility.log(PipeLogLevel.INFO, 'Test'))
+    .pipe(
+      Operators.Utility.log(
+        PipeLogLevel.INFO,
+        'Data for notifications pipeline',
+      ),
+    )
     .pipe(Operators.Transform.getRaw())
     .pipe(Operators.FlowControl.rateLimit(Duration.fromObject({ seconds: 20 })))
     .pipe(
@@ -16,11 +21,33 @@ const collateralizationRatioNotifications: EventDetectionPipeline<number> = (
         (cRatio: number) => `Your c-ratio is: ${cRatio}`,
       ),
     )
-    .pipe(Operators.Utility.log(PipeLogLevel.INFO, 'Event'));
+    .pipe(Operators.Utility.log(PipeLogLevel.INFO, 'Notification'));
+
+const collateralizationRatioWarnings: EventDetectionPipeline<number> = (
+  source,
+) =>
+  source
+    .pipe(
+      Operators.Utility.log(PipeLogLevel.INFO, 'Data for warnings pipeline'),
+    )
+    .pipe(Operators.Transform.getRaw())
+    .pipe(Operators.Window.fixedSize(3))
+    .pipe(Operators.Aggregate.avg())
+    .pipe(...Operators.Trigger.fallingEdge(20))
+    .pipe(
+      Operators.Event.info(
+        'C-ratio warning',
+        (cRatio: number) => `Your c-ratio is too low: ${cRatio}`,
+      ),
+    )
+    .pipe(Operators.Utility.log(PipeLogLevel.INFO, 'Warning'));
 
 export const jetEventDetectionPipelines: Record<
   ParameterId,
-  EventDetectionPipeline<number>
+  EventDetectionPipeline<number>[]
 > = Object.fromEntries([
-  [C_RATIO_PARAMETER_ID, collateralizationRatioNotifications],
+  [
+    C_RATIO_PARAMETER_ID,
+    [collateralizationRatioNotifications, collateralizationRatioWarnings],
+  ],
 ]);
