f172d5845f3ead9730910caae0cd9b0afd462f6d
Alexey Tsymbal <alexey@gotiggy.com>
2022-02-08 02:01:55 +0300

    Use correct thresholds in jet pipelines

--- src/server/jet-pipeline.ts
+++ src/server/jet-pipeline.ts
@@ -7,38 +7,38 @@ import {
   SubscriberEvent,
 } from '@dialectlabs/monitor';
 import { C_RATIO_PARAMETER_ID } from './jet-data-sources';
-import { Duration } from 'luxon';
 
 setPipeLogLevel(PipeLogLevel.INFO);
 
-const collateralizationRatioNotifications: EventDetectionPipeline<number> = (
-  source,
-) =>
+const liquidationWarningThreshold = 125; // based on https://docs.jetprotocol.io/jet-protocol/beginner-guides/understanding-c-ratio-and-liquidation
+const liquidationWarnings: EventDetectionPipeline<number> = (source) =>
   source
     .pipe(Operators.Utility.log(PipeLogLevel.INFO))
     .pipe(Operators.Transform.getRaw())
-    .pipe(Operators.FlowControl.rateLimit(Duration.fromObject({ seconds: 20 })))
+    .pipe(Operators.Window.fixedSize(5))
+    .pipe(Operators.Aggregate.avg())
+    .pipe(...Operators.Trigger.fallingEdge(liquidationWarningThreshold))
     .pipe(
-      Operators.Event.info(
-        'C-ratio info',
-        (cRatio: number) => `Your c-ratio is: ${cRatio}`,
+      Operators.Event.warning(
+        'C-ratio warning',
+        (cRatio: number) =>
+          `Your C-ratio ${cRatio}% is unhealthy. A portion of your collateral will is liquidated to maintain a healthy C-ratio, since it dropped below ${liquidationWarningThreshold}%.`,
       ),
     )
     .pipe(Operators.Utility.log(PipeLogLevel.INFO));
 
-const collateralizationRatioWarnings: EventDetectionPipeline<number> = (
-  source,
-) =>
+const unhealthyCRatioWarningThreshold = 150; // based on https://docs.jetprotocol.io/jet-protocol/beginner-guides/understanding-c-ratio-and-liquidation
+const unhealthyCRatioWarnings: EventDetectionPipeline<number> = (source) =>
   source
-    .pipe(Operators.Utility.log(PipeLogLevel.INFO))
     .pipe(Operators.Transform.getRaw())
-    .pipe(Operators.Window.fixedSize(3))
+    .pipe(Operators.Window.fixedSize(5))
     .pipe(Operators.Aggregate.avg())
-    .pipe(...Operators.Trigger.fallingEdge(20))
+    .pipe(...Operators.Trigger.fallingEdge(unhealthyCRatioWarningThreshold))
     .pipe(
-      Operators.Event.info(
+      Operators.Event.warning(
         'C-ratio warning',
-        (cRatio: number) => `Your c-ratio is too low: ${cRatio}`,
+        (cRatio: number) =>
+          `Your C-ratio ${cRatio}% is unhealthy. A portion of your collateral will be liquidated to maintain a healthy C-ratio when it drops below ${liquidationWarningThreshold}%.`,
       ),
     )
     .pipe(Operators.Utility.log(PipeLogLevel.INFO));
@@ -47,17 +47,13 @@ export const jetUnicastMonitorPipelines: Record<
   ParameterId,
   EventDetectionPipeline<number>[]
 > = Object.fromEntries([
-  [
-    C_RATIO_PARAMETER_ID,
-    [collateralizationRatioNotifications, collateralizationRatioWarnings],
-  ],
+  [C_RATIO_PARAMETER_ID, [liquidationWarnings, unhealthyCRatioWarnings]],
 ]);
 
 const welcomeMessagePipeline: EventDetectionPipeline<SubscriberEvent> = (
   source,
 ) =>
   source
-    .pipe(Operators.Utility.log(PipeLogLevel.INFO))
     .pipe(
       Operators.Transform.filter(
         ({ parameterData: { data } }) => data === 'added',
@@ -67,7 +63,7 @@ const welcomeMessagePipeline: EventDetectionPipeline<SubscriberEvent> = (
       Operators.Event.info(
         'Welcome',
         () => `Thanks for subscribing for Jet Notifications (managed by Dialect). 
-You'll receive daily notifications about your collateralization ratio & risk of liquidation warnings.`,
+You'll receive notifications about your collateralization ratio & risk of liquidation warnings.`,
       ),
     )
     .pipe(Operators.Utility.log(PipeLogLevel.INFO));
